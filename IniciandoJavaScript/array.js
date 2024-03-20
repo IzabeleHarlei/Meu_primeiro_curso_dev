5572c10be5f54581f6358ce57954a111d519e28c
jg2ol <159553167+IzabeleHarlei@users.noreply.github.com>
2024-03-20 11:54:56 -0300

    array
    cont

---
+++ IniciandoJavaScript/array.js
@@ -0,0 +1,28 @@
+const trabalho = [
+   {
+    nome: "Thaís",
+    idade: 19,
+    profissao: "Enfermeira"},
+    {
+     nome : "Michael",
+     idade : 38,
+     profissao: "Programador front-end"
+    },
+    {
+     nome : "Jamile",
+     idade : 20,
+     profissao: "Engenheira Aeroespacial"
+    },
+    {
+     nome : "Emília",
+     idade : 20,
+     profissao : "Cirurgiã Plástica"
+    },
+    {
+     nome: "Marcus",
+     idade: 18,
+     profissao: "Professor de História"
+    }
+]
+console.log(trabalho[3].nome, trabalho[3].idade, trabalho[3].profissao)
+
