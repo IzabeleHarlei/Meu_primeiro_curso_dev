5b5e0a822d175e23860ffa9c890e3388f07999a0
IzabeleHarlei <159553167+IzabeleHarlei@users.noreply.github.com>
2024-04-19 19:23:20 -0300

    aposentadoria
    atividade javascript

---
+++ IniciandoJavaScript/atvaposentadoria.js
@@ -0,0 +1,24 @@
+const nome = "Silvana";
+const idade = 48;
+const sexo = "F";
+const contribuição = 23;
+
+if( sexo = "F"){
+    if(contribuição >= 30){
+        if(idade + contribuição >= 85){
+            console.log("${nome}, a senhora pode se aposentar!")
+        }else{
+            console.log("${nome}, a senhora ainda não pode se aposentar!")
+        }
+        }
+    }
+else{
+    if(contribuição >= 35){
+        if(idade + contribuição >= 95){
+            console.log("${nome}, o senhor pode se aposentar pode se aposentar!")
+        }else{
+            console.log("${nome}, o senhor ainda não pode se aposentar!")
+
+        }
+    }
+}
